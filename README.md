ca6e6dadb21c2fa73dc95f1f939dc8639379204f
Nate Babyak <nate.babyak@outlook.com>
2026-05-26 23:53:36 -0400

    feat: add containermcstack to cdk-stack.ts

--- README.md
+++ README.md
@@ -2,18 +2,16 @@
 
 The easiest way to deploy a Minecraft server in the cloud.
 
-## Project Structure
+## Getting Started
 
-```text
-ContainerMC
-├── cdk
-│   ├── 
-│   ├── 
-│   ├── 
-│   └── 
-├── web
-│   ├── 
-│   ├── 
-│   └── 
-└── README.md
-```
+For non-technical users, head to [containermc.com](https://containermc.com) to get started.
+
+### For Technical Users
+
+## Acknowledgements
+
+ContainerMC relies on countless open-source projects. Here are some of the key ones:
+
+- [aws-cdk](https://github.com/aws/aws-cdk)
+- [itzg/minecraft-server](https://github.com/itzg/docker-minecraft-server)
+- [Svelte](https://github.com/sveltejs/svelte)
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,108 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as logs from "aws-cdk-lib/aws-logs";
+import * as autoscaling from "aws-cdk-lib/aws-autoscaling";
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class ContainerMCStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      maxAzs: 2,
+      natGateways: 0,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // IAM role for EC2 instances
+    const instanceRole = new iam.Role(this, "InstanceRole", {
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+      managedPolicies: [
+        // ECS agent
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AmazonEC2ContainerServiceforEC2Role",
+        ),
+        // SSM for RCON
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "AmazonSSMManagedInstanceCore",
+        ),
+        // CloudWatch logs
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "CloudWatchAgentServerPolicy",
+        ),
+      ],
+    });
+
+    // Security group for EC2 instances
+    const instanceSg = new ec2.SecurityGroup(this, "InstanceSg", {
+      vpc,
+      description: "Minecraft EC2 instances",
+    });
+    instanceSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(25565),
+      "Minecraft Java",
+    );
+    instanceSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.udp(19132),
+      "Minecraft Bedrock",
+    );
+
+    // ECS cluster
+    const cluster = new ecs.Cluster(this, "Cluster", { vpc });
+
+    // Auto Scaling Group with t4g instances
+    const asg = new autoscaling.AutoScalingGroup(this, "ASG", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T4G,
+        ec2.InstanceSize.XLARGE,
+      ),
+      machineImage: ecs.EcsOptimizedImage.amazonLinux2(ecs.AmiHardwareType.ARM),
+      minCapacity: 1,
+      maxCapacity: 20,
+      role: instanceRole,
+      securityGroup: instanceSg,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      associatePublicIpAddress: true,
+    });
+
+    // Attach ASG to ECS cluster
+    const capacityProvider = new ecs.AsgCapacityProvider(this, "AsgCp", {
+      autoScalingGroup: asg,
+      enableManagedScaling: true,
+      enableManagedTerminationProtection: false,
+    });
+    cluster.addAsgCapacityProvider(capacityProvider);
+
+    // CloudWatch log group
+    const logGroup = new logs.LogGroup(this, "ServerLogs", {
+      logGroupName: "/containermc/servers",
+      retention: logs.RetentionDays.ONE_MONTH,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // IAM role for ECS tasks (what the container can do)
+    const taskRole = new iam.Role(this, "TaskRole", {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+    });
+
+    // allow tasks to write to CloudWatch
+    logGroup.grantWrite(taskRole);
+
+    // outputs
+    new cdk.CfnOutput(this, "ClusterArn", { value: cluster.clusterArn });
+    new cdk.CfnOutput(this, "ClusterName", { value: cluster.clusterName });
+    new cdk.CfnOutput(this, "VpcId", { value: vpc.vpcId });
+    new cdk.CfnOutput(this, "InstanceSgId", {
+      value: instanceSg.securityGroupId,
+    });
+    new cdk.CfnOutput(this, "LogGroupName", { value: logGroup.logGroupName });
+    new cdk.CfnOutput(this, "TaskRoleArn", { value: taskRole.roleArn });
+    new cdk.CfnOutput(this, "CapacityProviderName", {
+      value: capacityProvider.capacityProviderName,
+    });
   }
 }
