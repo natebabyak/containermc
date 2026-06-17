a7f15e89346c3f546573829926b59b457951fa01
Nate Babyak <nate.babyak@outlook.com>
2026-06-17 17:33:13 -0400

    feat: add start server button to session page

--- web/src/routes/(protected)/org/[organizationSlug]/server/[minecraftServerSlug]/sessions/+page.svelte
+++ web/src/routes/(protected)/org/[organizationSlug]/server/[minecraftServerSlug]/sessions/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { resolve } from '$app/paths';
+	import { enhance } from '$app/forms';
+	import { page } from '$app/state';
 	import { Badge } from '$lib/components/ui/badge/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Empty from '$lib/components/ui/empty/index.js';
@@ -69,10 +70,21 @@
 						<Panda />
 					</Empty.Media>
 					<Empty.Title>No sessions</Empty.Title>
-					<Empty.Description>Sessions are</Empty.Description>
+					<Empty.Description>Sessions are created when your server is started.</Empty.Description>
 				</Empty.Header>
 				<Empty.Content>
-					<Button>Start Server</Button>
+					<form
+						method="post"
+						action="/org/{page.params.organizationSlug}/servers?/startMinecraftServer"
+						use:enhance={() => {
+							return async ({ update }) => {
+								update();
+							};
+						}}
+					>
+						<input type="hidden" name="minecraftServerId" value={page.params.minecraftServerId} />
+						<Button type="submit">Start Server</Button>
+					</form>
 				</Empty.Content>
 			</Empty.Root>
 		{/each}
