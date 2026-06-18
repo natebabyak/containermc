412b6b2d90e7216300a7bf57d7984108584f014b
Nate Babyak <nate.babyak@outlook.com>
2026-06-17 23:28:20 -0400

    feat: enhance server management page with start/stop buttons and backup restoration options

--- web/src/routes/(protected)/org/[organizationSlug]/server/[minecraftServerSlug]/+page.svelte
+++ web/src/routes/(protected)/org/[organizationSlug]/server/[minecraftServerSlug]/+page.svelte
@@ -9,6 +9,8 @@
 	import type { DragEndEvent, DragOverEvent } from '@dnd-kit/abstract';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import Play from '@lucide/svelte/icons/play';
+	import * as ButtonGroup from '$lib/components/ui/button-group/index.js';
+	import Square from '@lucide/svelte/icons/square';
 
 	let { data }: LayoutProps = $props();
 
@@ -33,44 +35,51 @@
 <svelte:head>
 	<title>{data.activeMinecraftServer.name} - ContainerMC</title>
 </svelte:head>
-<div class="space-y-4 p-4">
+<div class="space-y-4">
 	<InputGroup.Root>
 		<InputGroup.Input placeholder="Enter command..." />
 		<InputGroup.Addon>
 			<SquareTerminal />
 		</InputGroup.Addon>
 	</InputGroup.Root>
-	<DragDropProvider {onDragEnd}>
-		<Item.Root>
-			<Item.Header>
-				<Item.Media class="row-span-2">
-					<img src={data.activeMinecraftServer.iconUrl} alt="server icon" class="size-16" />
-				</Item.Media>
-				<Item.Title>{data.activeMinecraftServer.name}</Item.Title>
-				<Item.Description>server motd goes here</Item.Description>
-				<span>Player count</span>
-			</Item.Header>
-			<Item.Content>
+	<Item.Root variant="outline">
+		<Item.Header>
+			<Item.Media class="row-span-2">
+				<img src={data.activeMinecraftServer.iconUrl} alt="server icon" class="size-16" />
+			</Item.Media>
+			<Item.Title>{data.activeMinecraftServer.name}</Item.Title>
+			<Item.Description>server motd goes here</Item.Description>
+			<span>Player count</span>
+		</Item.Header>
+		<Item.Content></Item.Content>
+		<Item.Footer>
+			<ButtonGroup.Root>
 				<Button>
 					<Play />
 					Start
 				</Button>
-			</Item.Content>
-		</Item.Root>
-		<Item.Root variant="outline">
-			<Item.Header>
-				<Item.Title>Logs</Item.Title>
-			</Item.Header>
-			<Item.Content>
-				{#each logs as log, i (i)}
-					<pre>{log}</pre>
-				{/each}
-			</Item.Content>
-		</Item.Root>
-		<DragDropProvider {onDragStart} {onDragOver} {onDragEnd}>
+				<Button>
+					<Square />
+					Stop
+				</Button>
+			</ButtonGroup.Root>
+		</Item.Footer>
+	</Item.Root>
+	<Item.Root variant="outline">
+		<Item.Header>
+			<Item.Title>Logs</Item.Title>
+		</Item.Header>
+		<Item.Content>
+			{#each logs as log, i (i)}
+				<pre>{log}</pre>
+			{/each}
+		</Item.Content>
+	</Item.Root>
+	<DragDropProvider {onDragStart} {onDragOver} {onDragEnd}>
+		<div class="grid-col-1 grid gap-4 lg:grid-cols-2">
 			{#each items as id, index (id)}
 				<SortableItem {id} {index} />
 			{/each}
-		</DragDropProvider>
+		</div>
 	</DragDropProvider>
 </div>
--- web/src/routes/(protected)/org/[organizationSlug]/server/[minecraftServerSlug]/backups/+page.svelte
+++ web/src/routes/(protected)/org/[organizationSlug]/server/[minecraftServerSlug]/backups/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Item from '$lib/components/ui/item/index.js';
+	import { formatTime } from '$lib/formatters';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
@@ -8,10 +10,24 @@
 <h1>Backups</h1>
 <Item.Group>
 	{#each data.activeMinecraftServer.backups as backup (backup.id)}
-		<Item.Root>
+		<Item.Root variant="outline">
 			<Item.Content>
-				{backup.minecraftServerId}
+				<Item.Title>
+					{data.activeMinecraftServer.name} at {formatTime(backup.createdAt)}
+				</Item.Title>
+				<Item.Description>
+					{new Intl.NumberFormat('en-US', {
+						style: 'unit',
+						unit: 'byte',
+						unitDisplay: 'long'
+					}).format(backup.sizeBytes)}
+				</Item.Description>
 			</Item.Content>
+			<Item.Actions>
+				<Button class="border-primary bg-linear-to-b from-white/20 via-primary to-black/20">
+					Restore
+				</Button>
+			</Item.Actions>
 		</Item.Root>
 	{/each}
 </Item.Group>
