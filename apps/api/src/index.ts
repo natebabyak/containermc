7308f1b0d35d7050e1a9bb9575490eabb6711827
Nate Babyak <nate.babyak@outlook.com>
2026-08-12 00:10:55 -0400

    feat: add auth schema

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,21 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { auth } from "./lib/auth.js";
 
 const app = new Hono();
 
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:3000",
+    allowMethods: ["POST", "GET"],
+  }),
+);
+
 app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
-serve(app);
+serve({
+  fetch: app.fetch,
+  port: 4666,
+});
--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { emailOTPClient, organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3001",
   plugins: [emailOTPClient(), organizationClient(), stripeClient()],
 });
